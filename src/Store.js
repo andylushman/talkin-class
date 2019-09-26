d7a349a547a7911f5e9b026f4e0fcc39f21afc4f
andylushman <andylushman@gmail.com>
2019-09-26 09:16:04 -0600

    Andy to User

--- src/Store.js
+++ src/Store.js
@@ -5,28 +5,28 @@ export const CTX = React.createContext();
 
 const initialState = {
   general: [
-    { from: 'Andy1', msg: 'Anyone know of an education conference this week?' },
-    { from: 'Andy2', msg: 'Yeah, I think there is one in Denver' }
+    { from: 'User1', msg: 'Anyone know of an education conference this week?' },
+    { from: 'User2', msg: 'Yeah, I think there is one in Denver' }
   ],
   students: [
-    { from: 'Andy1', msg: 'Where can I contact my coach?' },
-    { from: 'Andy2', msg: 'You can find the number on the website' }
+    { from: 'User1', msg: 'Where can I contact my coach?' },
+    { from: 'User2', msg: 'You can find the number on the website' }
   ],
   schools: [
-    { from: 'Andy1', msg: 'Seeing a lot of new students here on campus!' },
-    { from: 'Andy2', msg: 'That is great!' }
+    { from: 'User1', msg: 'Seeing a lot of new students here on campus!' },
+    { from: 'User2', msg: 'That is great!' }
   ],
   advisors: [
-    { from: 'Andy1', msg: 'Enrollment is up this semester!' },
-    { from: 'Andy2', msg: 'Awesome!' }
+    { from: 'User1', msg: 'Enrollment is up this semester!' },
+    { from: 'User2', msg: 'Awesome!' }
   ],
   companies: [
     {
-      from: 'Andy1',
+      from: 'User1',
       msg:
         'We have 90% of our employees taking advantage of our educational benefits!'
     },
-    { from: 'Andy2', msg: 'Nice! Hopefully we will hit 100% soon ;)' }
+    { from: 'User2', msg: 'Nice! Hopefully we will hit 100% soon ;)' }
   ]
 };
 
@@ -48,7 +48,7 @@ let socket;
 function sendChatAction(value) {
   socket.emit('chat message', value);
 }
-const user = 'Andy' + Math.random(100).toFixed(2);
+const user = 'User' + Math.random(100).toFixed(2);
 
 export default function Store(props) {
   const [allChats, dispatch] = React.useReducer(reducer, initialState);
